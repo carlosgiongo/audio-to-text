3da8fe803cfedf0302cdba840f31f89dbaff0ff5
Carlos Giongo <carlosgiongoo@gmail.com>
2025-03-15 13:48:26 -0300

    feat: adicionado suporte a base64

--- index.js
+++ index.js
@@ -13,13 +13,31 @@ app.use(express.json());
 
 app.post("/extract-text", async (req, res) => {
   const fileUrl = req.body.url;
+  const fileBase64 = req.body.base64;
+  const auth = req.headers["authorization"];
 
-  if (!fileUrl) {
-    return res.status(400).json({ error: "Missing 'url' in request body" });
+  if(auth !== process.env.AUTHORIZATION) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  if (!fileUrl && !fileBase64) {
+    return res.status(400).json({ error: "Missing 'url' or 'base64' in request body" });
   }
 
   try {
-    const response = await axios.get(fileUrl, { responseType: "arraybuffer" });
+    let response;
+
+    if (fileUrl) {
+      response = await axios.get(fileUrl, { responseType: "arraybuffer" });
+    }
+
+    if (fileBase64) {
+      response = {
+        data: Buffer.from(fileBase64, "base64"),
+        headers: { "content-type": "audio/mpeg" },
+      };
+    }
+
     const audioData = Buffer.from(response.data, "binary");
     const mimeType = response.headers["content-type"];
 
